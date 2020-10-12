ba604e0cb55e58fff91f74e601b18271a9f056c3
Unnatural16 <1796472245@qq.com>
2020-10-12 14:26:07 +0800

    22

--- 22.js
+++ 22.js
@@ -1,24 +1,47 @@
 /**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
+ * @param {number[][]} matrix
+ * @return {number[]}
  */
-/**
- * @param {ListNode} head
- * @param {number} k
- * @return {ListNode}
- */
-var getKthFromEnd = function(head, k) {
-    let fast=head
-    while (k-->0){
-        fast=fast.next
+var spiralOrder = function (matrix) {
+
+    let result = [];
+    //turn取值1,2,3,4:上，右，下，左
+    function code(x, y, turn) {
+        if(matrix.length==0) return [];
+        result.push(matrix[y][x])
+        matrix[y][x] = null
+        if (turn == 1) {
+            if (matrix[y - 1]&&matrix[y - 1][x] != null) {
+                code(x, y - 1, 1)
+            } else if (matrix[y]&&matrix[y][x + 1] != null) {
+                code(x + 1, y, 2)
+            } else {
+                return
+            }
+        } else if (turn == 2) {
+            if (matrix[y]&&matrix[y][x + 1] != null) {
+                code(x + 1, y, 2)
+            } else if (matrix[y+1] &&matrix[y + 1][x] != null) {
+                code(x, y + 1, 3)
+            } else {
+                return
+            }
+        } else if (turn == 3) {
+            if (matrix[y + 1]&&matrix[y + 1][x] != null) {
+                code(x, y + 1, 3)
+            } else if (matrix[y]&&matrix[y][x - 1] != null) {
+                code(x - 1, y, 4)
+            } else {
+                return false
+            }
+        } else {
+            if (matrix[y]&&matrix[y][x - 1] != null) {
+                code(x - 1, y, 4)
+            } else if (matrix[y - 1]&&matrix[y - 1][x] != null) {
+                code(x, y - 1, 1)
+            } else { return }
+        }
     }
-    let slow=head
-    while (fast!=null){
-        fast=fast.next
-        slow=slow.next
-    }
-    return slow
+    code(0,0,2)
+    return result
 };
