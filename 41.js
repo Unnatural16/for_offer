e04175b8bbedb4363ebd6797acd2dee7bd38887d
Unnatural16 <1796472245@qq.com>
2020-10-15 10:58:09 +0800

    41

--- 41.js
+++ 41.js
@@ -1,36 +1,43 @@
 /**
  * initialize your data structure here.
  */
-var MedianFinder = function() {
-    this.array=[]
+var MedianFinder = function () {
+    this.array = []
 };
 
 /** 
  * @param {number} num
  * @return {void}
  */
-MedianFinder.prototype.addNum = function(num) {
-    if(this.array.length==0){
+MedianFinder.prototype.addNum = function (num) {
+    if (this.array.length == 0) {
         this.array.push(num);
         return
     }
-    for(var i=0; i<this.array.length; i++){
-        if(this.array[i]>=num){
-            this.array.splice(i,0,num);
-            return
+    let left = 0,
+        right = this.array.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (this.array[mid] === num) {
+            this.array.splice(mid, 0, num);
+            return;
+        } else if (this.array[mid] < num) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
-    this.array.push(num);
+    this.array.splice(right + 1, 0, num);
 };
 
 /**
  * @return {number}
  */
-MedianFinder.prototype.findMedian = function() {
-    if(this.array.length%2==0){
-        return (this.array[this.array.length/2-1]+this.array[this.array.length/2])/2
-    }else{
-        return this.array[Math.floor(this.array.length/2)]
+MedianFinder.prototype.findMedian = function () {
+    if (this.array.length % 2 == 0) {
+        return (this.array[this.array.length / 2 - 1] + this.array[this.array.length / 2]) / 2
+    } else {
+        return this.array[Math.floor(this.array.length / 2)]
     }
 };
 
