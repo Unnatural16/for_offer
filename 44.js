33f9b8327bde6967debccb81b2bd41b12fd711c7
Unnatural16 <1796472245@qq.com>
2020-10-16 22:40:35 +0800

    44

---
+++ 44.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var findNthDigit = function(n) {
+    if(!n) return 0
+    let temp = n
+    for(var digits = 0; temp >0;digits++){
+        temp -= (digits+1)*9*Math.pow(10,digits)
+    }
+    temp+=(digits)*9*Math.pow(10,digits-1)
+    const idx=temp%digits
+    let number=Math.pow(10,digits-1)+Math.floor(temp/digits)
+    if(!idx){
+        number--
+        return number%10
+    }else{
+        return parseInt(number.toString()[idx-1])
+    }
+};
